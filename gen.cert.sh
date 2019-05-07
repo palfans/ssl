ea0f532cb07eaa1e011f7debe52eecd773d0d92e
palfans <palfans@gmail.com>
2019-05-07 17:17:44 -0500

    feat: add option of subject

--- gen.cert.sh
+++ gen.cert.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo
     echo 'Issue a wildcard SSL certificate with Fishdrowned ROOT CA'
     echo
-    echo 'Usage: ./gen.cert.sh <domain> [<domain2>] [<domain3>] [<domain4>] ...'
+    echo 'Usage: ./gen.cert.sh [-s /C=US/ST=Texas/L=Dallas/O=Palfans] <domain> [<domain2>] [<domain3>] [<domain4>] ...'
     echo '    <domain>          The domain name of your site, like "example.dev",'
     echo '                      you will get a certificate for *.example.dev'
     echo '                      Multiple domains are acceptable'
-    exit;
+    exit
 fi
 
 SAN=""
-for var in "$@"
-do
-    SAN+="DNS:*.${var},DNS:${var},"
+SUBJ=""
+DOMAIN=""
+while [ $# != 0 ]; do
+    if [ "$1" == "-s" ]; then
+        SUBJ=$2
+        shift 2
+    fi
+
+    if [ "${DOMAIN}" == "" ]; then
+        DOMAIN=$1
+    fi
+
+    SAN+="DNS:*.$1,DNS:$1,"
+    shift
 done
 SAN=${SAN:0:${#SAN}-1}
 
@@ -28,38 +38,37 @@ if [ ! -f "out/root.crt" ]; then
 fi
 
 # Create domain directory
-BASE_DIR="out/$1"
-TIME=`date +%Y%m%d-%H%M`
+BASE_DIR="out/${DOMAIN}"
+TIME=$(date +%Y%m%d-%H%M)
 DIR="${BASE_DIR}/${TIME}"
 mkdir -p ${DIR}
 
 # Create CSR
-openssl req -new -out "${DIR}/$1.csr.pem" \
-    -key out/cert.key.pem \
-    -reqexts SAN \
-    -config <(cat ca.cnf \
-        <(printf "[SAN]\nsubjectAltName=${SAN}")) \
-    -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/OU=$1/CN=*.$1"
+MSYS_NO_PATHCONV=1 openssl req -new -out "${DIR}/${DOMAIN}.csr.pem" \
+-key out/cert.key.pem \
+-reqexts SAN \
+-config <(cat ca.cnf <(printf "[SAN]\nsubjectAltName=${SAN}")) \
+-subj "${SUBJ}/OU=${DOMAIN}/CN=*.${DOMAIN}"
 
 # Issue certificate
 # openssl ca -batch -config ./ca.cnf -notext -in "${DIR}/$1.csr.pem" -out "${DIR}/$1.cert.pem"
 openssl ca -config ./ca.cnf -batch -notext \
-    -in "${DIR}/$1.csr.pem" \
-    -out "${DIR}/$1.crt" \
-    -cert ./out/root.crt \
-    -keyfile ./out/root.key.pem
+-in "${DIR}/${DOMAIN}.csr.pem" \
+-out "${DIR}/${DOMAIN}.crt" \
+-cert ./out/root.crt \
+-keyfile ./out/root.key.pem
 
 # Chain certificate with CA
-cat "${DIR}/$1.crt" ./out/root.crt > "${DIR}/$1.bundle.crt"
-ln -snf "./${TIME}/$1.bundle.crt" "${BASE_DIR}/$1.bundle.crt"
-ln -snf "./${TIME}/$1.crt" "${BASE_DIR}/$1.crt"
-ln -snf "../cert.key.pem" "${BASE_DIR}/$1.key.pem"
+cat "${DIR}/${DOMAIN}.crt" ./out/root.crt >"${DIR}/${DOMAIN}.bundle.crt"
+ln -snf "./${TIME}/${DOMAIN}.bundle.crt" "${BASE_DIR}/${DOMAIN}.bundle.crt"
+ln -snf "./${TIME}/${DOMAIN}.crt" "${BASE_DIR}/${DOMAIN}.crt"
+ln -snf "../cert.key.pem" "${BASE_DIR}/${DOMAIN}.key.pem"
 ln -snf "../root.crt" "${BASE_DIR}/root.crt"
 
 # Output certificates
 echo
 echo "Certificates are located in:"
 
-LS=$([[ `ls --help | grep '\-\-color'` ]] && echo "ls --color" || echo "ls -G")
+LS=$([[ $(ls --help | grep '\-\-color') ]] && echo "ls --color" || echo "ls -G")
 
-${LS} -la `pwd`/${BASE_DIR}/*.*
+${LS} -la $(pwd)/${BASE_DIR}/*.*
--- gen.root.sh
+++ gen.root.sh
@@ -11,10 +11,10 @@ if [ ! -d "out" ]; then
 fi
 
 # Generate root cert along with root key
-openssl req -config ca.cnf \
+MSYS_NO_PATHCONV=1 openssl req -config ca.cnf \
     -newkey rsa:2048 -nodes -keyout out/root.key.pem \
     -new -x509 -days 7300 -out out/root.crt \
-    -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/CN=Fishdrowned ROOT CA"
+    -subj "/C=US/ST=Texas/L=Dallas/O=Palfans/CN=Palfans ROOT CA"
 
 # Generate cert key
 openssl genrsa -out "out/cert.key.pem" 2048
